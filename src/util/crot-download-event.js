554ee2dc7975fed00ce553e66379d6ecbb8115a1
Diego Martins <dmartins@mail.com>
2023-03-27 18:24:54 -0300

    Lógica para renomear corretamente arquivos baixados pelo observer

--- src/util/crot-download-event.js
+++ src/util/crot-download-event.js
@@ -1,24 +1,33 @@
 import { DownloadEvent } from "./download-event.js";
 import { resolve } from "node:path";
-import { rmSync } from "node:fs";
+import { renameSync, rmSync } from "node:fs";
 
 export class CrotDownloadEvent extends DownloadEvent {
     /**
      * @param {import("../entity/account").Account} account
+     * @param {boolean} keepFileAfterRead
      */
-    constructor(account) {
+    constructor(account, keepFileAfterRead = false) {
+        super();
+
         this._account = account;
+        this._keepFileAfterRead = keepFileAfterRead;
     }
 
     async downloadCompleted(event) {
-        // aqui o arquivo vai ter o nome igual ao guid, não necessita renomear
+        // aqui o arquivo vai ter o nome igual ao guid, não necessita renomear imediatamente
         const basename = resolve(this._downloadPath, event.guid);
 
         // subir o arquivo no spaces
         // use os dados em `this._account`
         //uploadAttachPdf()
 
-        // apagar o arquivo ao final
-        rmSync(basename);
+        // apagar, ou não, o arquivo ao final
+        if (!this._keepFileAfterRead) {
+            rmSync(basename);
+            return;
+        }
+
+        renameSync(basename, resolve(this._downloadPath, `crot-${this._account.cpfCliente}.pdf`));
     }
 }
